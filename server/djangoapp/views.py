a13b38f39eba19aeeede6ed7891ffdf2d871f5d5
mlhadi <mlhadi1992@gmail.com>
2021-10-01 14:44:43 +0800

    Added login views and urls.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -29,10 +29,26 @@ def contact(request):
         return render(request, 'djangoapp/contact.html')
 
 
-
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['password']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # If not, return to login page again
+            return redirect('djangoapp:index')
+    else:
+        return redirect('djangoapp:index')
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
