238ccbc0589eb7613c4b7bbbd710751927b825f5
mlhadi <mlhadi1992@gmail.com>
2021-10-01 22:54:25 +0800

    Added CarMake and CareModel models. Updated admin page settings.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=280)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +25,29 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealerid = models.IntegerField()
+    name = models.CharField(max_length=30)
+
+    SEDAN = 'SD'
+    SUV = 'SUV'
+    WAGON = 'WAG'
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+    car_type = models.CharField(
+        max_length=3,
+        choices=CAR_TYPE_CHOICES,
+        default=SEDAN
+    )
+
+    year = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.make
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
