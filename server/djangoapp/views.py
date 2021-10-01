e13288e24cf6a49dedf62edcbd53ea37ff3c57ab
mlhadi <mlhadi1992@gmail.com>
2021-10-01 14:46:23 +0800

    Added registration views and urls.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -57,8 +57,31 @@ def logout_request(request):
 
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == 'POST':
+        username = request.POST['inputusername']
+        password = request.POST['inputpassword']
+        first_name = request.POST['inputfirstname']
+        last_name = request.POST['inputlastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is new user".format(username))
+        if not user_exist:
+            user = User.objects.create_user(
+                username=username, first_name=first_name, last_name=last_name,
+                password=password
+            )
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return render(request, 'djangoapp/registration.html', context)
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
