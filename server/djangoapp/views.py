a0ec8c76ecd6077b906a213257f628b7e32c3c1a
mlhadi <mlhadi1992@gmail.com>
2021-10-02 12:24:26 +0800

    Added views to display results extracted from serverless/Cloudant.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer
+from .restapis import get_dealers_from_cf, get_dealer_by_state_from_cf
+from .restapis import get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -85,16 +86,55 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://bd541e50.us-south.apigw.appdomain.cloud/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
+
+
+def get_dealerships_by_state(request, state):
+    if request.method == "GET":
+        url = "https://bd541e50.us-south.apigw.appdomain.cloud/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealer_by_state_from_cf(url, state)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == 'GET':
+        url = 'https://bd541e50.us-south.apigw.appdomain.cloud/api/review'
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+        dealer_reviews = '\n'. join(f'{review.review} {review.sentiment}' for review in reviews)
+        return HttpResponse(dealer_reviews)
+
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    if request.user.is_authenticated:
+        review = {
+            "name": "Berkly Shepley",
+            "dealership": dealer_id,
+            "review": "This is a test review",
+            "purchase": True,
+            "purchase_date": datetime.now().strftime('%F'),
+            "car_make": "Audi",
+            "car_model": "A6",
+            "car_year": 2010
+        }
+        url = 'https://bd541e50.us-south.apigw.appdomain.cloud/api/review'
+        json_payload = dict(review=review)
+        post_request(url, json_payload, dealerId=dealer_id)
+        return HttpResponse(review)
+    else:
+        return redirect('djangoapp:about')
+
+
 
